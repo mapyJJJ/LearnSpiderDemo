b563d1d291b094863c85606699dc2104e46ba613
mapyJJJ <wszsdpyjjj@gmail.com>
2018-12-31 19:13:48 +0800

    2018-12-31修改提交，在windows上的chromedriver.exe问题修复（无法调用chrome的问题，pyvirtualdisplay无法在windwos上使用）

--- 电视剧地址采集器/main.py
+++ 电视剧地址采集器/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import webbrowser
-
 import PIL.Image
 from PIL import ImageTk
 from tkinter import *
@@ -15,53 +14,70 @@ from menubar import *
 #2,获取免费的vip接口
 #主窗口
 
-class Video_APP():
+class Video_APP():   #主页面定义Tk界面
     def __init__(self):
-        self.title="vip视频在线观看"
+        #定义类变量，项目标题 作者 版本号
+        self.title="vip视频在线观看"    
         self.author="金牛小子"
         self.version='V1.0'
+
         self.finally_video_url=''
+
+        # 创建Tk容器（标题定义为 title） 同时还有一个 顶部的菜单
         self.root=Tk(className=self.title)
         self.menubar=Menu(self.root)
         self.root.resizable(0, 0)
 
+        # 定义三个tk下的三个字符串类型
         self.moive_title=StringVar()
         self.moive_star=StringVar()
         self.t_c_item=StringVar()
 
+        #先将他们赋值为空
+        self.moive_title,self.moive_star,self.t_c_item = '','',''
+
+        #定义菜单栏，并且使用config将它们固定到窗口上
         self.menubar.add_command(label="使用必看",command=gui_info)
         self.menubar.add_command(label="环境配置", command=check)
         self.root.config(menu=self.menubar)
 
+        #开始划分我们的整个界面
         self.farme_left=Frame(self.root)
         #左边框架
         #左上图片展示
+        #先使用PIL.Image.open()打开图片文件
         im=PIL.Image.open("download.png")
+        #然后可以定义Image.PhotoImage()放上我们的图片
         self.photo=ImageTk.PhotoImage(im)
+        #插入一个标签，将我们的图片放到标签里面显示
         self.photolable=Label(self.farme_left,image=self.photo)
         self.photolable.pack(padx=2,pady=0)
-        #图片下面的详情页介绍  简介需限制长度
+        #图片下面的详情页介绍  简介需限制长度，不然会挤掉我们的窗口
         self.text_label1=Label(self.farme_left,text=self.moive_title,font=("宋体",20),width=26,wraplength=300)
         self.text_label1.pack(anchor=N)
         self.text_label2=Label(self.farme_left,text=self.moive_star,justify=CENTER,font=("宋体",15),width=26,wraplength=300)
         self.text_label2.pack(anchor=N)
         self.text_label3=Label(self.farme_left,text=self.t_c_item,justify=CENTER,font=("宋体",15),width=26,wraplength=300)
         self.text_label3.pack(anchor=N)
+        #最后我们闭合farme ，并且让它靠左显示
         self.farme_left.pack(side=LEFT,padx=0)
         #########################################
 
+        #创建一个frame_right 用来显示右边的内容
         farme_right = Frame(self.root)
-        # 右边框架
-
+        #我们又可以把frame_right也进行一个划分，这里的是farme_right_one
         farme_right_one=Frame(farme_right)
+        #顶部的是线路选择的功能
         Label(farme_right_one,text="选择线路:",font=("宋体",13),fg="green").pack(side=LEFT,anchor=NW,padx=10,pady=10)
         ROAD = [("线路一", 1), ("线路二", 2), ("线路三", 3), ("线路四", 4)]
+        #设置四个我们需要选择后赋值的选项 v --代表接口  w --代表链接来源  x --影视剧的类型  t -- 是否随机选择获取的的链接
         self.v,self.w,self.x,self.t= IntVar(),IntVar(),IntVar(),IntVar()
         self.v.set(1)
         for lang, num in ROAD:
             Radiobutton(farme_right_one, text=lang, variable=self.v, value=num,font=("宋体",12)).pack(pady=10,padx=10,side=LEFT)
         farme_right_one.pack(side=TOP)
 
+        #再划分出来一个 farme_right_two (这里是手动复制链接，然后拼接解析接口，打开浏览器自动播放)
         farme_right_two=Frame(farme_right)
         Label(farme_right_two,text="输入视频网址:",fg="green",font=("宋体",13)).pack(anchor=NW,side=LEFT,padx=7,pady=20)
         self.website_url=StringVar()
@@ -69,6 +85,7 @@ class Video_APP():
         Button(farme_right_two, text="开始播放",command=self.play).pack(side=LEFT,padx=7,pady=20)
         farme_right_two.pack(side=TOP)
 
+        #继续划分 出 farme_right_three
         farme_right_three=Frame(farme_right)
         Label(farme_right_three,text="播放来源:",fg="green",font=("楷书",13)).pack(padx=10,pady=10,anchor=NW,side=LEFT)
         self.w.set(2)
@@ -77,6 +94,7 @@ class Video_APP():
             Radiobutton(farme_right_three, text=lang, variable=self.w, value=num,font=("宋体",12),command=self.choose_source).pack(padx=5,pady=0,side=LEFT)
         farme_right_three.pack(side=TOP)
 
+        #划分出 farme_right_five
         farme_right_five = Frame(farme_right)
         self.video_name=StringVar()
         Label(farme_right_five, text="剧集名称:", fg="green", font=("宋体", 12)).pack(anchor=NW,side=LEFT,padx=17,pady=10)
@@ -101,11 +119,15 @@ class Video_APP():
         farme_right_choose.pack(side=TOP)
 
         farme_right_four=Frame(farme_right)
+        #定义sb 滑动条，在y坐标轴上
         sb=Scrollbar(farme_right_four)
         sb.pack(side=RIGHT,fill=Y)
+        #定义一个Listbox 用于显示链接结果
         self.lb=Listbox(farme_right_four,yscrollcommand=sb.set,width=40,height=13)
+        #在表单中插入‘准备就绪’
         self.lb.insert(END,"准备就绪!")
         self.lb.pack(side=LEFT,fill=BOTH)
+        #将表单与滑动条连接在一起
         sb.config(command=self.lb.yview)
         farme_right_four.pack(side=LEFT,padx=17,pady=10)
 
@@ -120,6 +142,7 @@ class Video_APP():
         sb.config(command=self.lb1.yview)
         farme_right_six.pack(pady=0)
 
+        #底部的一些功能按钮
         farme_right_seven=Frame(farme_right)
         Button(farme_right_seven, text="联系作者",width=20,command=self.contact_me).pack(side=TOP)
         Button(farme_right_seven, text="系统设置",width=20,command=setting).pack(side=TOP)
@@ -129,9 +152,12 @@ class Video_APP():
         farme_right.pack()
 
         mainloop()
+
+    #联系作者的函数
     def contact_me(self):
         webbrowser.open("https://github.com/mapyJJJ/100-")
 
+    #根据
     def spider_n_to_u(self):
         choose_num=self.choose_btn()   #获取用户选择(匹配第一个或是随机)
         self.name=self.video_name.get()     #获取用户输入的名称
--- 电视剧地址采集器/test_selenium.py
+++ 电视剧地址采集器/test_selenium.py
@@ -5,18 +5,19 @@ from pyvirtualdisplay import Display
 
 #尝试自动化解决集数过多的剧集
 def selenium_parse(finally_video_url):
-    display = Display(visible=0, size=(800, 600))
-    display.start()
     #browser=webdriver.PhantomJS()
     #尝试使用chromedriver驱动（需要用户自己安装）
     try:
-        browser=webdriver.Chrome()
-    except:
+        #无窗口的selenium自动化获取
+        opt = webdriver.ChromeOptions()
+        opt.set_headless()
+        browser=webdriver.Chrome(options=opt)
+    except:  #适用于在linux下的自动化获取
         options = webdriver.ChromeOptions()
-        options.add_argument("--no-sandbox")
-        browser=webdriver.Chrome(chrome_options=options)
-    browser.get(finally_video_url)
-    browser.find_element_by_link_text("全部").click()
+        options.add_argument("--no-sandbox")  #linux 下需要使用--no-sandbox 选项来调用chrome
+        browser=webdriver.Chrome(chrome_options=options) 
+    browser.get(finally_video_url)  #打开剧集详情页面
+    browser.find_element_by_link_text("全部").click()  #点击全部按钮，展示每一集的链接
     sleep(0.5)
     soup=BeautifulSoup(browser.page_source,'lxml')
     url=[]
@@ -28,6 +29,5 @@ def selenium_parse(finally_video_url):
     reverse_url_list=url[0:int(find_site)]
     reverse_url_list.reverse()
     reverse_url_list.insert(0,"[+]成功获取到完整地址")
-    browser.quit()
-    display.stop()
-    return reverse_url_list
+    browser.quit()  #退出selenium虚拟浏览器
+    return reverse_url_list  #返回我们获取到的完整的链接
